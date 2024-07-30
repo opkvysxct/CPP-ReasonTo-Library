c23dc53789bd6f320568500e9751dd3318187f46
oskar2115 <oskarpiotrkraw@gmail.com>
2024-07-31 01:46:10 +0200

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,43 @@
+#include "library.h"
+
+#include <iostream>
+#include <vector>
+
+class ReasonTo {
+public:
+	int anyReasons;
+	std::vector<std::string> specificReasons;
+	bool CanProceed();
+	void AddAnyReason();
+	void AddSpecificReason(std::string reasonName);
+	void RemoveAnyReason();
+	void RemoveSpecificReason(std::string reasonName);
+	ReasonTo();
+	~ReasonTo();
+};
+
+bool ReasonTo::CanProceed() {
+	if (this->anyReasons == 0 && this->specificReasons.size() == 0) {
+		return true;
+	}
+	return false;
+}
+
+void ReasonTo::AddAnyReason() {
+	this->anyReasons++;
+}
+
+void ReasonTo::RemoveAnyReason() {
+	if (this->anyReasons>1)	this->anyReasons--;
+}
+
+void ReasonTo::AddSpecificReason(std::string reasonName) {
+	this->specificReasons.push_back(reasonName);
+}
+
+void ReasonTo::RemoveSpecificReason(std::string reasonName) {
+	this->specificReasons.erase(std::find(
+			this->specificReasons.begin(),
+			this->specificReasons.end(),
+			reasonName));
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef REASONTO_LIBRARY_H
+#define REASONTO_LIBRARY_H
+
+class ReasonTo;
+
+#endif //REASONTO_LIBRARY_H
